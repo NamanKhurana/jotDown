f8801b67ad9dcc703da5db5a761a61e010cd165e
NamanKhurana <khurananaman06@gmail.com>
2019-04-19 14:11:05 +0530

    initial commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production')
 {
-    module.exports = {mongoURI: 'mongodb+srv://Naman:Nk@vidjot-cluster-6cbrv.mongodb.net/test?retryWrites=true'}
+    module.exports = {mongoURI: 'mongodb://Naman:Nk@vidjot-cluster-shard-00-00-6cbrv.mongodb.net:27017,vidjot-cluster-shard-00-01-6cbrv.mongodb.net:27017,vidjot-cluster-shard-00-02-6cbrv.mongodb.net:27017/test?ssl=true&replicaSet=VidJot-Cluster-shard-0&authSource=admin&retryWrites=true'}
 }
 else
 {
